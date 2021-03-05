d419078392e50cc4f824e680c9d345b08c9c1e75
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-05 22:51:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,16 @@
 class Author
   
-  attr_accessor :name, :posts
+  attr_accessor :name
   
   @@post_count = 0
   
   def initialize(name)
     @name = name
     @posts = []
+  end
+  
+  def posts
+    @posts
   end
   
   def add_post(post)
